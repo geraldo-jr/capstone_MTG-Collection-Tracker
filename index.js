3c85c36b8a0e520a7317176fa30ac99dd67ee913
geraldo-jr <49133473+geraldo-jr@users.noreply.github.com>
2022-04-15 08:32:32 -0500

    Test response for / route and initial index.js code

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ const pool = new Pool({
     rejectUnauthorized: false
   }
 });
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.json())
+  .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      
+      client.release();
+      res.send("MTG Collection Tracker Works!");
+    } catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+      
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
