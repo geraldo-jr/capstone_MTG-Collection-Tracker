83c07f54d5cce8b7beb2251c806fc7f3e14496a9
geraldo-jr <49133473+geraldo-jr@users.noreply.github.com>
2022-04-19 10:44:52 -0500

    [finishes #181873295] Implements user sign up and show the right data on the db-info

--- index.js
+++ index.js
@@ -19,10 +19,12 @@ express()
   .get('/', async(req, res) => {
     try {
       const client = await pool.connect();
-      
+
+      res.render('pages/index');
+
       client.release();
-      res.send("MTG Collection Tracker Works!");
     } catch (err) {
+
       console.error(err);
       res.send("Error: " + err);
       
@@ -38,21 +40,67 @@ express()
 				ON c.oid = a.attrelid AND a.attnum > 0
 				LEFT JOIN pg_catalog.pg_type AS t
 				ON a.atttypid = t.oid
-				WHERE c.relname IN ('users', 'observations', 'students', 'schools', 'tasks')
+				WHERE c.relname IN ('users')
 				ORDER BY c.relname, a.attnum;`
 			);
+
+      const users = await client.query(
+        `SELECT last_name, uusername FROM users;`
+      );
       
       const locals = {
-        'tables': (tables) ? tables.rows : null
+        'tables': (tables) ? tables.rows : null,
+        'users': (users) ? users.rows : null
       };
 
       res.render('pages/db-info', locals);
       client.release();
 
-    } catch (error) {
+    } catch (err) {
+      console.error(err);
+      res.send("Error: " + err);
+    }
+  })
+  .post('/log', async(req, res) => {
+    try {
+      const client = await pool.connect();
+      const userPassword = req.body.password;
+      const userUsername = req.body.username;
+      const userEmail = req.body.email;
+      const userFirst_name = req.body.first_name;
+      const userLast_name = req.body.last_name;
       
+      console.log(userPassword + " " + userUsername + " " + userEmail + " " + userFirst_name + " " + userLast_name);
+      const sql = `INSERT INTO users (password, uusername, email, first_name, last_name)
+      VALUES (${userPassword}, ${userUsername}, ${userEmail}, ${userFirst_name}, ${userLast_name})
+      RETURNING user_id as new_id;`;
+      console.log(sql);
+
+      const sqlInsert = await client.query(
+        `INSERT INTO users (user_id, password, uUsername, email, first_name, last_name)
+        VALUES (123, '${userPassword}', '${userUsername}', '${userEmail}', '${userFirst_name}', '${userLast_name}')
+        RETURNING user_id as new_id;`);
+
+      console.log(sqlInsert);
+
+      // console.log(`Tracking task ${userId}`);
+
+      const result = {
+        'respose': (sqlInsert) ? (sqlInsert.rows[0]) : null
+      };
+      res.set({
+        'Content-Type': 'applicatio/json'
+      });
+      res.json({ requestBody: result });
+      client.release();
+    } catch (err) {
       console.error(err);
       res.send("Error: " + err);
     }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+  function catchError(err) {
+    console.error(err);
+    res.send("Error: " + err);
+  }
